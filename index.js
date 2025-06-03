a42bf75fe5b5d15ac3ffecebfce3bef25a8a31db
Cyprian <lightningcyranus@gmail.com>
2025-06-03 09:51:13 +0200

    improved fix

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let currentUser = 'Grace Hopper';
 
-const welcomeMessage = "Welcome to Flatbook, Grace Hopper!";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, GRACE HOPPER!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = "Welcome, G!";
+
+
+const shortGreeting =  `Welcome, ${currentUser.slice(0, 1)}!`;
 
